16ccf2ef6eea5fdb8892f336b85aeef821a80305
Serafim <nesk@xakep.ru>
2017-08-04 02:19:45 +0300

    Implement arguments

---
+++ src/Reflection/Argument.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * This file is part of Railgun package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Serafim\Railgun\Reflection;
+
+use Hoa\Compiler\Llk\TreeNode;
+use Serafim\Railgun\Reflection\Abstraction\ArgumentInterface;
+use Serafim\Railgun\Reflection\Abstraction\DocumentTypeInterface;
+use Serafim\Railgun\Reflection\Abstraction\NamedDefinitionInterface;
+use Serafim\Railgun\Reflection\Abstraction\Type\TypeInterface;
+use Serafim\Railgun\Reflection\Common\Directives;
+use Serafim\Railgun\Reflection\Common\HasLinkingStageInterface;
+use Serafim\Railgun\Reflection\Common\HasName;
+use Serafim\Railgun\Reflection\Common\LinkingStage;
+use Serafim\Railgun\Reflection\Type\ListType;
+use Serafim\Railgun\Reflection\Type\RelationType;
+
+/**
+ * Class Argument
+ * @package Serafim\Railgun\Reflection
+ */
+class Argument extends Definition implements
+    HasLinkingStageInterface,
+    ArgumentInterface
+{
+    use HasName;
+    use Directives;
+    use LinkingStage;
+
+    /**
+     * @var NamedDefinitionInterface
+     */
+    private $parent;
+
+    /**
+     * @var TypeInterface
+     */
+    private $type;
+
+    /**
+     * @var mixed
+     */
+    private $defaultValue;
+
+    /**
+     * @var bool
+     */
+    private $hasDefaultValue = false;
+
+    /**
+     * Argument constructor.
+     * @param DocumentTypeInterface $document
+     * @param TreeNode $ast
+     * @param NamedDefinitionInterface $parent
+     * @throws \LogicException
+     */
+    public function __construct(DocumentTypeInterface $document, TreeNode $ast, NamedDefinitionInterface $parent)
+    {
+        parent::__construct($document, $ast);
+
+        $this->parent = $parent;
+
+        $this->bootHasName($document, $ast);
+        $this->bootLinkingStage($document, $ast);
+
+        $this->compileIfNotCompiled();
+    }
+
+    /**
+     * @return TypeInterface
+     */
+    public function getType(): TypeInterface
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasDefaultValue(): bool
+    {
+        return $this->hasDefaultValue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDefaultValue()
+    {
+        return $this->defaultValue;
+    }
+
+    /**
+     * @return NamedDefinitionInterface
+     */
+    public function getParent(): NamedDefinitionInterface
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param Document $document
+     * @param TreeNode $ast
+     * @return TreeNode|null
+     * @throws \LogicException
+     */
+    public function compile(Document $document, TreeNode $ast): ?TreeNode
+    {
+        switch ($ast->getId()) {
+            case '#List':
+                $this->type = new ListType($this->document, $ast);
+                break;
+            case '#Type':
+                $this->type = new RelationType($this->document, $ast);
+                break;
+            case '#Value':
+                $this->hasDefaultValue = true;
+                $this->defaultValue = Value::new($document, $ast);
+        }
+
+        return $ast;
+    }
+}
