06765500fb11af731d111b33000b2a4cb6d3d7e6
Serafim <nesk@xakep.ru>
2017-08-10 22:56:19 +0300

    Add a little more exceptions

---
+++ src/Exceptions/GraphQLSchemaException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of Railgun package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Serafim\Railgun\Exceptions;
+
+/**
+ * Interface GraphQLSchemaException
+ * @package Serafim\Railgun\Exceptions
+ */
+interface GraphQLSchemaException
+{
+    /**
+     * @return int
+     */
+    public function getCodeColumn(): int;
+
+    /**
+     * @return int
+     */
+    public function getCodeLine(): int;
+}
--- src/Exceptions/UnrecognizedTokenException.php
+++ src/Exceptions/UnrecognizedTokenException.php
@@ -16,9 +16,19 @@ use Serafim\Railgun\Compiler\File;
  * Class UnrecognizedTokenException
  * @package Serafim\Railgun\Exceptions
  */
-class UnrecognizedTokenException extends \ParseError
+class UnrecognizedTokenException extends \ParseError implements GraphQLSchemaException
 {
     use RailgunException;
+
+    /**
+     * @var int
+     */
+    protected $codeLine = 0;
+
+    /**
+     * @var int
+     */
+    protected $codeColumn = 0;
 
     /**
      * @param UnrecognizedToken|\Exception $parent
@@ -27,7 +37,29 @@ class UnrecognizedTokenException extends \ParseError
      */
     public static function fromHoa(UnrecognizedToken $parent, File $file): UnrecognizedTokenException
     {
-        return static::new($parent->getMessage())
-            ->from($parent)->in($file->getPathname());
+        $self = static::new($parent->getMessage())
+            ->in($file->getPathname(), $parent->getLine())
+            ->from($parent);
+
+        $self->codeLine = $parent->getLine();
+        $self->codeColumn = $parent->getColumn();
+
+        return $self;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCodeColumn(): int
+    {
+        return $this->codeColumn;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCodeLine(): int
+    {
+        return $this->codeLine;
     }
 }
