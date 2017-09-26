fe3f7476d7be0b10b286ff10429691a10e024a13
Serafim <nesk@xakep.ru>
2017-09-26 22:27:16 +0300

    Add enum builder

---
+++ src/Railt/Reflection/Builder/EnumBuilder.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of Railt package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Railt\Reflection\Builder;
+
+use Hoa\Compiler\Llk\TreeNode;
+use Railt\Reflection\Base\BaseEnum;
+use Railt\Reflection\Builder\Support\Builder;
+
+/**
+ * Class EnumBuilder
+ */
+class EnumBuilder extends BaseEnum implements Compilable
+{
+    use Builder;
+
+    /**
+     * EnumBuilder constructor.
+     * @param TreeNode $ast
+     * @param DocumentBuilder $document
+     * @throws \Railt\Reflection\Exceptions\TypeConflictException
+     */
+    public function __construct(TreeNode $ast, DocumentBuilder $document)
+    {
+        $this->bootBuilder($ast, $document);
+    }
+
+    /**
+     * @param TreeNode $ast
+     * @return bool
+     * @throws \LogicException
+     */
+    public function compile(TreeNode $ast): bool
+    {
+        throw new \LogicException(__METHOD__ . ' not implemented yet');
+    }
+}
