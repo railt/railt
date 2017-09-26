62c5bc6bb63adbdf7e1f3a81745129bcee92d056
Serafim <nesk@xakep.ru>
2017-09-26 22:27:02 +0300

    Add directive builder

---
+++ src/Railt/Reflection/Builder/DirectiveBuilder.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of Railt package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Railt\Reflection\Builder;
+
+use Hoa\Compiler\Llk\TreeNode;
+use Railt\Reflection\Base\BaseDirective;
+use Railt\Reflection\Builder\Support\ArgumentsBuilder;
+use Railt\Reflection\Builder\Support\Builder;
+
+/**
+ * Class DirectiveBuilder
+ */
+class DirectiveBuilder extends BaseDirective implements Compilable
+{
+    use Builder;
+    use ArgumentsBuilder;
+
+    /**
+     * DirectiveBuilder constructor.
+     * @param TreeNode $ast
+     * @param DocumentBuilder $document
+     * @throws \Railt\Reflection\Exceptions\TypeConflictException
+     */
+    public function __construct(TreeNode $ast, DocumentBuilder $document)
+    {
+        $this->bootBuilder($ast, $document);
+    }
+
+    /**
+     * @param TreeNode $ast
+     * @return bool
+     */
+    public function compile(TreeNode $ast): bool
+    {
+        switch ($ast->getId()) {
+            case '#Target':
+                /** @var TreeNode $child */
+                foreach ($ast->getChild(0)->getChildren() as $child) {
+                    $this->locations[] = \strtoupper($child->getValueValue());
+                }
+        }
+
+        return false;
+    }
+}
