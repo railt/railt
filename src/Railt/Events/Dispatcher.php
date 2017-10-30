543d51b4011ba63f6be4c6639fc9015e46a52467
Serafim <nesk@xakep.ru>
2017-10-31 00:35:52 +0300

    Add events interface

--- src/Railt/Events/Dispatcher.php
+++ src/Railt/Events/Dispatcher.php
@@ -10,105 +10,34 @@ declare(strict_types=1);
 namespace Railt\Events;
 
 /**
- * Class Dispatcher
+ * The Dispatcher is an elementary event system often
+ * used as a mediator within the project.
  */
-class Dispatcher implements DispatcherInterface
+interface Dispatcher
 {
     /**
-     * @var array|\Closure[][]
-     */
-    private $listeners = [];
-
-    /**
-     * @var array
-     */
-    private $keys = [];
-
-    /**
-     * @param string $key
-     */
-    private function prepare(string $key): void
-    {
-        if (!array_key_exists($key, $this->listeners)) {
-            $this->listeners[$key] = [];
-        }
-    }
-
-    /**
-     * @param string $name
-     * @return array
-     */
-    public function events(string $name): array
-    {
-        $key = $this->key($name);
-
-        $this->prepare($key);
-
-        return $this->listeners[$key];
-    }
-
-    /**
-     * @param string $name
+     * Subscribe to a specific event type. The `*` symbol
+     * means any text in the event name.
+     *
+     * If the "\Closure" returns something other than `null` (void),
+     * then further subscribers will not be called. That is, if two
+     * listeners (subscribers) were created and the closure of the first
+     * returns, for example, falsity, then the second subscriber will
+     * not be called.
+     *
+     * @param string $event
      * @param \Closure $then
-     * @return DispatcherInterface|Dispatcher
+     * @return Dispatcher
      */
-    public function listen(string $name, \Closure $then): DispatcherInterface
-    {
-        $key = $this->key($name);
-
-        $this->prepare($key);
-
-        $this->listeners[$key][] = $then;
-
-        return $this;
-    }
+    public function listen(string $event, \Closure $then): Dispatcher;
 
     /**
-     * @param string $name
+     * Calling a specific event (subscribers to an event)
+     * with a strictly defined name.
+     *
+     * @param string $event
      * @param mixed $payload
      * @return mixed
      */
-    public function dispatch(string $name, $payload)
-    {
-        foreach ($this->find($name) as $listener) {
-            $result = $listener($name, $payload);
-
-            if ($result !== null) {
-                $payload = $result;
-            }
-        }
-
-        return $payload;
-    }
-
-    /**
-     * @param string $name
-     * @return \Traversable|\Closure[]
-     */
-    private function find(string $name):  \Traversable
-    {
-        foreach ($this->listeners as $key => $listeners) {
-            if (preg_match($key, $name)) {
-                foreach ((array)$listeners as $listener) {
-                    yield $name => $listener;
-                }
-            }
-        }
-    }
-
-    /**
-     * @param string $name
-     * @return string
-     */
-    private function key(string $name): string
-    {
-        if (!array_key_exists($name, $this->keys)) {
-            $regex = '/^' . preg_quote($name, '/') . '$/isu';
-            $regex = str_replace('\\*', '.+?', $regex);
-
-            $this->keys[$name] = $regex;
-        }
-
-        return $this->keys[$name];
-    }
+    public function dispatch(string $event, $payload);
 }
