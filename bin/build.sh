3147e2a999e6f40b8ed111154162f8ea995e67d1
Serafim <nesk@xakep.ru>
2018-01-10 07:21:25 +0300

    Remove routing

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 git subsplit init git@github.com:railt/railt.git
-git subsplit publish --heads="master 1.0" --no-tags src/Container:git@github.com:railt/container.git
-git subsplit publish --heads="master 1.0" --no-tags src/Http:git@github.com:railt/http.git
-git subsplit publish --heads="master 1.0" --no-tags src/Compiler:git@github.com:railt/compiler.git
-git subsplit publish --heads="master 1.0" --no-tags src/Routing:git@github.com:railt/routing.git
-git subsplit publish --heads="master 1.0" --no-tags src/Events:git@github.com:railt/events.git
-git subsplit publish --heads="master 1.0" --no-tags src/Reflection:git@github.com:railt/reflection.git
-git subsplit publish --heads="master 1.0" --no-tags src/GraphQL:git@github.com:railt/graphql.git
-git subsplit publish --heads="master 1.0" --no-tags src/Io:git@github.com:railt/io.git
-git subsplit publish --heads="master 1.0" --no-tags src/Adapters/Webonyx:git@github.com:railt/webonyx-adapter.git
+git subsplit publish --heads="master 1.x" --no-tags src/Container:git@github.com:railt/container.git
+git subsplit publish --heads="master 1.x" --no-tags src/Http:git@github.com:railt/http.git
+git subsplit publish --heads="master 1.x" --no-tags src/Compiler:git@github.com:railt/compiler.git
+git subsplit publish --heads="master 1.x" --no-tags src/Events:git@github.com:railt/events.git
+git subsplit publish --heads="master 1.x" --no-tags src/Reflection:git@github.com:railt/reflection.git
+git subsplit publish --heads="master 1.x" --no-tags src/GraphQL:git@github.com:railt/graphql.git
+git subsplit publish --heads="master 1.x" --no-tags src/Io:git@github.com:railt/io.git
+git subsplit publish --heads="master 1.x" --no-tags src/Adapters/Webonyx:git@github.com:railt/webonyx-adapter.git
 rm -rf .subsplit/
