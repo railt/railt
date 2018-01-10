4e7b6c3c02fccb977f983a01e82c0dddf6a10363
Serafim <nesk@xakep.ru>
2018-01-10 08:05:16 +0300

    Fix dependencies

---
+++ bin/release.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+VERSION="1.1.0"
+cd bin
+
+git clone git@github.com:railt/container.git
+cd container && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+git clone git@github.com:railt/http.git
+cd http && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+git clone git@github.com:railt/compiler.git
+cd compiler && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+git clone git@github.com:railt/events.git
+cd events && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+git clone git@github.com:railt/reflection.git
+cd reflection && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+git clone git@github.com:railt/graphql.git
+cd graphql && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+git clone git@github.com:railt/io.git
+cd io && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+git clone git@github.com:railt/webonyx-adapter.git
+cd webonyx-adapter && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
