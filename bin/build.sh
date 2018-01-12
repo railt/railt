dcd169a980b8ea34237a1c7edf93ac0ce9b03887
Serafim <nesk@xakep.ru>
2018-01-13 00:57:51 +0300

    Merge branch 'master' of github.com:railt/railt
    # Conflicts:
    #	composer.json
    #	composer.lock

--- bin/build.sh
+++ bin/build.sh
@@ -7,5 +7,6 @@ git subsplit publish --heads="master 1.x" --no-tags src/Events:git@github.com:ra
 git subsplit publish --heads="master 1.x" --no-tags src/Reflection:git@github.com:railt/reflection.git
 git subsplit publish --heads="master 1.x" --no-tags src/GraphQL:git@github.com:railt/graphql.git
 git subsplit publish --heads="master 1.x" --no-tags src/Io:git@github.com:railt/io.git
+git subsplit publish --heads="master 1.x" --no-tags src/Storage:git@github.com:railt/storage.git
 git subsplit publish --heads="master 1.x" --no-tags src/Adapters/Webonyx:git@github.com:railt/webonyx-adapter.git
 rm -rf .subsplit/
--- bin/release.sh
+++ bin/release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERSION="1.1.0"
+VERSION="1.2.0"
 cd bin
 
 git clone git@github.com:railt/container.git
@@ -23,6 +23,10 @@ cd graphql && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git 
 git clone git@github.com:railt/io.git
 cd io && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
 
-git clone git@github.com:railt/webonyx-adapter.git
-cd webonyx-adapter && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+git clone git@github.com:railt/storage.git
+cd storage && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
+
+# Not yet =)
+# git clone git@github.com:railt/webonyx-adapter.git
+# cd webonyx-adapter && git pull && git tag -a ${VERSION} -m "Release v${VERSION}" && git push origin ${VERSION} && cd ../
 
