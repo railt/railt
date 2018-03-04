7d5551f9b1d2b23d678bdba15748c84e59e4a0a5
Kirill Nesmeyanov <nesk@xakep.ru>
2018-03-04 12:16:04 +0300

    Apply fixes from StyleCI (#152)

--- src/Storage/Drivers/Psr6Storage.php
+++ src/Storage/Drivers/Psr6Storage.php
@@ -50,8 +50,7 @@ class Psr6Storage implements Storage
         CacheItemPoolInterface $pool,
         \Closure $persist,
         int $timeout = self::DEFAULT_REMEMBER_TIME
-    )
-    {
+    ) {
         $this->pool    = $pool;
         $this->persist = $persist;
         $this->timeout = $timeout;
