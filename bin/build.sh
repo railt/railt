f8ed582a0cb31b4321957b60440b95b501012f36
Serafim <nesk@xakep.ru>
2018-04-19 03:47:07 +0300

    Remove io from subsplit

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,5 @@ git subsplit publish --heads="master 1.x" --no-tags src/SDL:git@github.com:railt
 git subsplit publish --heads="master 1.x" --no-tags src/Reflection:git@github.com:railt/reflection.git
 git subsplit publish --heads="master 1.x" --no-tags src/Container:git@github.com:railt/container.git
 git subsplit publish --heads="master 1.x" --no-tags src/Http:git@github.com:railt/http.git
-git subsplit publish --heads="master 1.x" --no-tags src/Io:git@github.com:railt/io.git
 git subsplit publish --heads="master 1.x" --no-tags src/Storage:git@github.com:railt/storage.git
 rm -rf .subsplit/
