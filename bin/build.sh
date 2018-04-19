7fa5ce2f0eb1dfad554354d3fc9a260f0b8d76c1
Serafim <nesk@xakep.ru>
2018-04-19 04:41:00 +0300

    Move http to external package

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,5 @@ git subsplit init git@github.com:railt/railt.git
 git subsplit publish --heads="master 1.x" --no-tags src/SDL:git@github.com:railt/sdl.git
 git subsplit publish --heads="master 1.x" --no-tags src/Reflection:git@github.com:railt/reflection.git
 git subsplit publish --heads="master 1.x" --no-tags src/Container:git@github.com:railt/container.git
-git subsplit publish --heads="master 1.x" --no-tags src/Http:git@github.com:railt/http.git
 git subsplit publish --heads="master 1.x" --no-tags src/Storage:git@github.com:railt/storage.git
 rm -rf .subsplit/
