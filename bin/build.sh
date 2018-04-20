14f3ed9509894c9ff187173b2d2b54e69bf740e8
Serafim <nesk@xakep.ru>
2018-04-21 02:23:43 +0300

    Fix tests

--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,4 @@ git subsplit init git@github.com:railt/railt.git
 git subsplit publish --heads="master 1.x" --no-tags src/SDL:git@github.com:railt/sdl.git
 git subsplit publish --heads="master 1.x" --no-tags src/Reflection:git@github.com:railt/reflection.git
 git subsplit publish --heads="master 1.x" --no-tags src/Container:git@github.com:railt/container.git
-git subsplit publish --heads="master 1.x" --no-tags src/Storage:git@github.com:railt/storage.git
 rm -rf .subsplit/
