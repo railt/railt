76a55e1bbc491eeb9f56d6facfd3fba223352cae
Serafim <nesk@xakep.ru>
2018-04-21 02:41:33 +0300

    Split container from application

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,4 @@
 git subsplit init git@github.com:railt/railt.git
 git subsplit publish --heads="master 1.x" --no-tags src/SDL:git@github.com:railt/sdl.git
 git subsplit publish --heads="master 1.x" --no-tags src/Reflection:git@github.com:railt/reflection.git
-git subsplit publish --heads="master 1.x" --no-tags src/Container:git@github.com:railt/container.git
 rm -rf .subsplit/
