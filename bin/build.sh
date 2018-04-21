0ab222b88a57770c69f17a95d023d7659395a7ce
Serafim <nesk@xakep.ru>
2018-04-21 03:45:43 +0300

    Remove reflection from application

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 git subsplit init git@github.com:railt/railt.git
 git subsplit publish --heads="master 1.x" --no-tags src/SDL:git@github.com:railt/sdl.git
-git subsplit publish --heads="master 1.x" --no-tags src/Reflection:git@github.com:railt/reflection.git
 rm -rf .subsplit/
