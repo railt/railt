218189c0d1ea6dc7b7378c06a1b94a444f93817f
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2019-01-24 20:11:34 +0300

    Fix "use" directives logic

--- src/ClassLoader/DirectiveClassLoader.php
+++ src/ClassLoader/DirectiveClassLoader.php
@@ -9,11 +9,10 @@ declare(strict_types=1);
 
 namespace Railt\ClassLoader;
 
-use Railt\SDL\Contracts\Definitions\Definition;
+use Railt\Container\ContainerInterface;
+use Railt\Container\SignatureResolver;
 use Railt\SDL\Contracts\Document;
-use Railt\SDL\Contracts\Invocations\DirectiveInvocation;
 use Railt\SDL\Schema\CompilerInterface;
-use Railt\SDL\Schema\Configuration;
 
 /**
  * Class DirectiveClassLoader
@@ -21,176 +20,53 @@ use Railt\SDL\Schema\Configuration;
 class DirectiveClassLoader implements ClassLoaderInterface
 {
     /**
-     * @var CompilerInterface|Configuration
+     * @var SignatureResolver
      */
-    private $compiler;
+    private $signature;
 
     /**
-     * @var array|string
+     * @var Repository
      */
-    private $global = [];
-
-    /**
-     * @var array|string[]
-     */
-    private $documents = [];
-
-    /**
-     * @var bool
-     */
-    private $booted = false;
+    private $repository;
 
     /**
      * DirectiveClassLoader constructor.
      *
      * @param CompilerInterface $compiler
+     * @param ContainerInterface $container
      */
-    public function __construct(CompilerInterface $compiler)
+    public function __construct(CompilerInterface $compiler, ContainerInterface $container)
     {
-        $this->compiler = $compiler;
+        $this->repository = new Repository($compiler);
+        $this->signature = new SignatureResolver($container);
     }
 
     /**
      * @param Document $document
      * @param string $needle
      * @return string
+     * @throws \InvalidArgumentException
      */
     public function find(Document $document, string $needle): string
     {
-        $this->bootIfNotBooted();
-        $this->loadGlobalDirectives($document);
+        $aliases = $this->repository->getAliases($document);
+        $alias = $this->prefix($this->signature->fetchClass($needle));
 
-        $needleAlias = $this->unpackPrefix($needle);
-
-        foreach ($this->getAllowedClasses($document) as $alias => $relation) {
-            if ($alias === $needleAlias) {
-                return $this->replacePrefix($alias, $relation, $needle);
-            }
+        if (isset($aliases[$alias])) {
+            $needle = \substr_replace($needle, $aliases[$alias], 0, \strlen($alias));
         }
 
         return $needle;
     }
 
     /**
-     * @return void
-     */
-    private function bootIfNotBooted(): void
-    {
-        if ($this->booted === false) {
-            /** @var Definition $definition */
-            foreach ($this->compiler->getDictionary() as $definition) {
-                $this->loadGlobalDirectives($definition->getDocument());
-            }
-
-            $this->booted = true;
-        }
-    }
-
-    /**
-     * @param Document $document
-     */
-    private function loadGlobalDirectives(Document $document): void
-    {
-        $hash = $document->getFile()->getHash();
-
-        if (\in_array($hash, $this->documents, true)) {
-            return;
-        }
-
-        $this->documents[] = $hash;
-
-        foreach ($this->getUseDirectives($document) as $use) {
-            if ($this->isGlobal($use)) {
-                [$alias, $class] = $this->read($use);
-
-                $this->global[$alias] = $class;
-            }
-        }
-    }
-
-    /**
-     * @param Document $document
-     * @return iterable|DirectiveInvocation[]
-     */
-    private function getUseDirectives(Document $document): iterable
-    {
-        return $document->getDirectives('use');
-    }
-
-    /**
-     * @param DirectiveInvocation $directive
-     * @return bool
-     */
-    private function isGlobal(DirectiveInvocation $directive): bool
-    {
-        return $directive->getPassedArgument('scope') === 'GLOBAL';
-    }
-
-    /**
-     * @param DirectiveInvocation $directive
-     * @return array
-     */
-    private function read(DirectiveInvocation $directive): array
-    {
-        [$class, $alias] = [
-            $directive->getPassedArgument('class'),
-            $directive->getPassedArgument('as'),
-        ];
-
-        return [$alias ?? $this->unpackSuffix($class), $class];
-    }
-
-    /**
      * @param string $class
      * @return string
      */
-    private function unpackSuffix(string $class): string
-    {
-        $parts = \explode('\\', $class);
-
-        return \end($parts);
-    }
-
-    /**
-     * @param string $class
-     * @return string
-     */
-    private function unpackPrefix(string $class): string
+    private function prefix(string $class): string
     {
         $parts = \explode('\\', $class);
 
         return \reset($parts);
-    }
-
-    /**
-     * @param Document $document
-     * @return iterable|string[]
-     */
-    private function getAllowedClasses(Document $document): iterable
-    {
-        foreach ($this->getUseDirectives($document) as $directive) {
-            [$alias, $class] = $this->read($directive);
-
-            yield $alias => $class;
-        }
-
-        yield from $this->global;
-    }
-
-    /**
-     * @param string $alias
-     * @param string $relation
-     * @param string $needle
-     * @return string
-     */
-    private function replacePrefix(string $alias, string $relation, string $needle): string
-    {
-        $needleChunks = \explode('\\', $needle);
-
-        if (\reset($needleChunks) === $alias) {
-            \array_shift($needleChunks);
-        }
-
-        return \implode('\\', \array_merge([$relation], $needleChunks));
     }
 }
