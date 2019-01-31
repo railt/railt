a89b7cdbf0f63c007d6a2c7069f9a5896c46119a
Serafim <nesk@xakep.ru>
2019-02-01 02:50:37 +0300

    Add currying helper

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 use Railt\Support\Debug\Debuggable;
 use Railt\Support\Iter;
+use Railt\Support\PartiallyAppliedFunction;
 use Railt\Support\TypeDumper;
 
 if (! \function_exists('\\iterable_map')) {
@@ -57,5 +58,76 @@ if (! \function_exists('\\is_debug')) {
     function is_debug(Debuggable $debuggable): bool
     {
         return $debuggable->isDebug();
+    }
+}
+
+
+if (! \defined('_')) {
+    /**
+     * The placeholder for the given applications argument.
+     */
+    \define('_', PartiallyAppliedFunction::PLACEHOLDER);
+}
+
+
+if (! \function_exists('\\curry')) {
+    /**
+     * @see lcurry
+     * {@inheritdoc}
+     * @return callable|PartiallyAppliedFunction
+     */
+    function curry($fn, ...$args): callable
+    {
+        return PartiallyAppliedFunction::new($fn)->curry(...$args);
+    }
+}
+
+
+if (! \function_exists('\\lcurry')) {
+    /**
+     * Returns a function from a function to which you can partially
+     * apply the required arguments from left to right.
+     *
+     * @param callable|\ReflectionFunctionAbstract $fn An applicant function
+     * @param array $args Set of arguments for left currying
+     * @return callable|PartiallyAppliedFunction Returns a partially applied function
+     */
+    function lcurry($fn, ...$args): callable
+    {
+        return PartiallyAppliedFunction::new($fn)->lcurry(...$args);
+    }
+}
+
+
+if (! \function_exists('\\rcurry')) {
+    /**
+     * Returns a function from a function to which you can partially
+     * apply the required arguments from right to left.
+     *
+     * @param callable|\ReflectionFunctionAbstract $fn An applicant function
+     * @param array $args Set of arguments for right currying
+     * @return callable|PartiallyAppliedFunction Returns a partially applied function
+     */
+    function rcurry($fn, ...$args): callable
+    {
+        return PartiallyAppliedFunction::new($fn)->rcurry(...$args);
+    }
+}
+
+
+if (! \function_exists('\\uncurry')) {
+    /**
+     * Unpacks the curried function and returns the result of this function.
+     *
+     * In the event that the function is not completed (that is, there are not
+     * enough arguments), it returns a normal closure object, with which you
+     * can supplement the missing argument.
+     *
+     * @param callable|Curried $fn An applicant php or curried function
+     * @return int|float|bool|string|resource|object|\Closure Function result or closure
+     */
+    function uncurry(callable $fn): \Closure
+    {
+        return ($fn instanceof Curried ? $fn : Curried::new($fn))->uncurry();
     }
 }
