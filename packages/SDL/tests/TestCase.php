503bb1f4e15ea46b010dc81e63b11ef0c34ee5ad
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2019-11-18 22:21:51 +0300

    Add type overrides test case

--- packages/SDL/tests/TestCase.php
+++ packages/SDL/tests/TestCase.php
@@ -11,11 +11,29 @@ declare(strict_types=1);
 
 namespace Railt\SDL\Tests\Unit;
 
+use Railt\SDL\Compiler;
+use Railt\SDL\DocumentInterface;
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Phplrt\Source\Exception\NotFoundException;
+use Phplrt\Source\Exception\NotReadableException;
 
 /**
  * Class TestCase
  */
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * @param string $source
+     * @param int $spec
+     * @return DocumentInterface
+     * @throws NotFoundException
+     * @throws NotReadableException
+     * @throws \Throwable
+     */
+    protected function compile(string $source, int $spec = Compiler::SPEC_RAILT): DocumentInterface
+    {
+        $compiler = new Compiler($spec);
+
+        return $compiler->compile($source);
+    }
 }
---
+++ packages/SDL/tests/TypeOverridesTestCase.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This file is part of Railt package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Railt\SDL\Tests\Unit;
+
+use Phplrt\Source\Exception\NotFoundException;
+use Phplrt\Source\Exception\NotReadableException;
+
+/**
+ * Class TypeOverridesTestCase
+ */
+class TypeOverridesTestCase extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function typeMixesDataProvider(): array
+    {
+        $result = [];
+
+        foreach ($this->typesDataProvider() as $name => [$source]) {
+            foreach ($this->typesDataProvider() as $withName => [$withSource]) {
+                $result[\sprintf('%s + %s', $name, $withName)] = [
+                    \vsprintf('%s %s', [
+                        $source,
+                        $withSource,
+                    ]),
+                ];
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public function typesDataProvider(): array
+    {
+        return [
+            'enum'      => ['enum Example'],
+            'input'     => ['input Example'],
+            'interface' => ['interface Example'],
+            'object'    => ['type Example'],
+            'scalar'    => ['scalar Example'],
+            'union'     => ['union Example'],
+        ];
+    }
+
+    /**
+     * @dataProvider typeMixesDataProvider
+     *
+     * @param string $source
+     * @return void
+     * @throws NotFoundException
+     * @throws NotReadableException
+     * @throws \Throwable
+     */
+    public function testTypeRedefineByAnotherType(string $source): void
+    {
+        $this->expectExceptionMessage('There can be only one type named Example');
+
+        $this->compile($source);
+    }
+
+    /**
+     * @dataProvider typesDataProvider
+     *
+     * @param string $source
+     * @return void
+     * @throws NotFoundException
+     * @throws NotReadableException
+     * @throws \Throwable
+     */
+    public function testTypeRedefineByDirective(string $source): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        $this->compile($source . ' directive @Example on FIELD');
+    }
+
+    /**
+     * @return void
+     * @throws NotFoundException
+     * @throws NotReadableException
+     * @throws \Throwable
+     */
+    public function testDirectiveRedefineByDirective(): void
+    {
+        $this->expectExceptionMessage('There can be only one directive named @Example');
+
+        $this->compile(\str_repeat('directive @Example on FIELD ', 2));
+    }
+}
